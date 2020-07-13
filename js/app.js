d321ccdc27fcc461f2d01663df182915146b0115
Matthew Holder <holdermatthew5@gmail.com>
2020-07-13 10:06:01 -0700

    changed js files to functions

---
+++ js/app.js
@@ -0,0 +1,38 @@
+function greetUser() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+
+    if (hourNow > 18) {
+        greeting = 'Good evening!';
+    } else if (hourNow > 12) {
+        greeting = 'Good afternoon!';
+    } else if (hourNow > 0) {
+        greeting = 'Good morning!';
+    }
+
+    return document.write('<h3>' + greeting + '</h3>');
+}
+
+function askName() {
+    var name = prompt('What\'s your name');
+
+    return document.write('<h3>' + name + '</h3>');
+}
+
+function askPreference() {
+    var preference = prompt('Which episode do you prefer? one, two, or three?')
+    var response;
+
+    if (preference === 'one') {
+        response = 'Episode 1 is the best!';
+    } else if (preference === 'two') {
+        response = 'Episode 2 is ok.';
+    } else if (preference === 'three') {
+        response = 'Oh...';
+    } else {
+        response = 'fine don\'t answer!'
+    }
+
+    return document.write('<h3>' + response + '</h3>');
+}
--- js/ask-name.js
+++
@@ -1,3 +0,0 @@
-var name = prompt('What\'s your name');
-
-document.write('<h3>' + name + '</h3>');
